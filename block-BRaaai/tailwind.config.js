7bab8197b31aeb7f83a76ec994ff348ad898bb26
Prethush <ktmirash49@.gmail.com>
2021-08-10 12:43:51 +0530

    JSX

---
+++ block-BRaaai/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        custom: "#272727"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
