015b39eaf3ed30818ffab0000ccd9815c3c80717
Prethush <ktmirash49@.gmail.com>
2021-08-10 15:14:31 +0530

    components and props

---
+++ block-BRaaak/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        custom: "#272727"
+      },
+
+      flex: {
+        "33": "0 1 31.5%"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
